52f57c0000b1e4345497724486251ed4483b61f9
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2021-06-15 16:34:01 +0300

    Ball class

---
+++ CollisionMechanics/Ball.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Drawing;
+
+namespace CollisionMechanics
+{
+    class Ball
+    {
+        public float Radius { get; set; }
+        public float Weight { get; set; }
+        public Vector2 Position { get; set; } = Vector2.Zero;
+        public Vector2 Velocity { get; set; } = Vector2.Zero;
+        public string Label { get; set; } = string.Empty;
+        public Color Color { get; set; }
+        public bool Mark { get; set; }
+
+        public void Go()
+        {
+            Position += Velocity;
+            Mark = false;
+        }
+    }
+}
