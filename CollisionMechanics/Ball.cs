d49446a106461bf06fde532f8497cf1696437d31
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2021-06-17 10:48:30 +0300

    Wall and CM classes

--- CollisionMechanics/Ball.cs
+++ CollisionMechanics/Ball.cs
@@ -3,25 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Numerics;
 using System.Drawing;
 
 namespace CollisionMechanics
 {
-    class Ball
+    class Ball : Point
     {
-        public float Radius { get; set; }
-        public float Weight { get; set; }
-        public Vector2 Position { get; set; } = Vector2.Zero;
-        public Vector2 Velocity { get; set; } = Vector2.Zero;
-        public string Label { get; set; } = string.Empty;
-        public Color Color { get; set; }
-        public bool Mark { get; set; }
+        double _radius = 1;
+        double _weight = 1;
+        Vector _velocity = new Vector(1, 1);
+        bool _bMark = false;
+        public Color _color = Color.Black;
+        long _id = 0;
+
+        public long id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public Color Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+        public bool Mark
+        {
+            get { return _bMark; }
+            set { _bMark = value; }
+        }
+
+        public double R
+        {
+            get { return _radius; }
+            set { _radius = value; }
+        }
+
+        public double Weight
+        {
+            get { return _weight; }
+            set { _weight = value; }
+        }
+
+        public Vector Velocity
+        {
+            get { return _velocity; }
+            set { _velocity = value; }
+        }
+
+        public double KineticEnergy
+        {
+            get
+            {
+                double len = _velocity.Length;
+                return _weight * len * len / 2;
+            }
+        }
 
         public void Go()
         {
-            Position += Velocity;
-            Mark = false;
+            _X += _velocity.X;
+            _Y += _velocity.Y;
+
+            _bMark = false;
         }
     }
 }
