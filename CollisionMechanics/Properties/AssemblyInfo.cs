7fabdc8fd1f86dae4418609262da3e4a3afcd473
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2021-07-30 13:45:52 +0300

    Intro

--- CollisionMechanics/Properties/AssemblyInfo.cs
+++ CollisionMechanics/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CollisionMechanics")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DigitalModels")]
 [assembly: AssemblyProduct("CollisionMechanics")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © DigitalModels 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
